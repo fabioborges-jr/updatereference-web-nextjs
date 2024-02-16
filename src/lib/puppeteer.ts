3159cb082510db299b6106d22ee056bac87a8f68
Fabio <fabioborgesouzajunior@gmail.com>
2024-02-16 19:50:47 -0300

    :sparkles: feat: Creates Url references object

--- src/lib/puppeteer.ts
+++ src/lib/puppeteer.ts
@@ -1,5 +1,12 @@
 import puppeteer from 'puppeteer'
 
+const Url = {
+  sinapi: process.env.SINAPI_URL,
+  sudecap: process.env.SINAPI_URL,
+  setop: process.env.SINAPI_URL,
+  sicro: process.env.SINAPI_URL,
+}
+
 export async function searchElements() {
   const browser = await puppeteer.launch()
   const page = await browser.newPage()
