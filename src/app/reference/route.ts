e070480eacef72d1d8aa2c166524fe3739cd55f9
Fabio <fabioborgesouzajunior@gmail.com>
2024-02-17 23:36:20 -0300

    :sparkles: feat: Adds clickAcceptCookie()

--- src/app/reference/route.ts
+++ src/app/reference/route.ts
@@ -1,48 +1,49 @@
 import puppeteer, { Page } from 'puppeteer'
 
 type url = {
-  sinapi: string
-  sudecap: string
-  setop: string
-  sicro: string
+  sinapi?: string
+  sudecap?: string
+  setop?: string
+  sicro?: string
 }
 
-const url = {
+const url: url = {
   sinapi: process.env.SINAPI_URL,
   sudecap: process.env.SINAPI_URL,
   setop: process.env.SINAPI_URL,
   sicro: process.env.SINAPI_URL,
 }
 
-async function getListSinapiAllStates(page: Page) {
-  if (url.sinapi) await page.goto(url.sinapi)
-  await page.waitForSelector('::-p-text("SINAPI – a partir Jul/2009")')
-  const listSinapiAllStates = await page.$$eval(
-    '::-p-text("SINAPI – a partir Jul/2009")',
-    (references) => {
-      return references.map((reference) => reference.textContent)
-    },
+async function clickAcceptCookie(page: Page) {
+  await page.waitForSelector('#adopt-accept-all-button')
+  await page.click('#adopt-accept-all-button')
+}
+
+async function clickAllSinapiState(page: Page) {
+  await page.waitForSelector('::-p-text(SINAPI – a partir Jul/2009)')
+  const statesSelectors = await page.$$eval(
+    '::-p-text(SINAPI – a partir Jul/2009)',
+    (states) => states.map((state) => state.id),
   )
-  return listSinapiAllStates
+  for (const state of statesSelectors) {
+    await page.click(`#${state}`)
+    await getAllAElements(page)
+  }
+  console.log(`A quantidade de elementos é: ${statesSelectors}`)
+  const contentPage = await page.content()
+  return contentPage
+}
+
+async function getAllAElements(page: Page) {
+  await page.waitForSelector('::-p-text(SINAPI_ref_Insumos_Composicoes)')
 }
 
 export async function GET() {
-  const browser = await puppeteer.launch()
+  const browser = await puppeteer.launch({ headless: false })
   const page = await browser.newPage()
+  await page.goto('https://www.caixa.gov.br/site/Paginas/downloads.aspx')
+  await clickAcceptCookie(page)
+  const listSinapiAC = await clickAllSinapiState(page)
 
-  const gettedListSinapiAllStates = await getListSinapiAllStates(page)
-
-  // listSinapiAllStates.map(async (stateSinapi) => {
-  //   if (stateSinapi) {
-  //     await page.click(stateSinapi)
-  //     await page.waitForSelector('#btncategoria_639')
-  //     const listSinapiState = await page.$$eval(
-  //       '::p-text("SINAPI_ref_Insumos_Composicoes_AC")',
-  //       (monthsState) => {
-  //         return monthsState.map((monthState) => monthState.textContent)
-  //       },
-  //     )
-  //   }
-  // })
-  return Response.json(gettedListSinapiAllStates)
+  return Response.json(listSinapiAC)
 }
