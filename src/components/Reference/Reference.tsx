dfb7915d6bcfdae3a390d852451089648d5e5fa3
Fabio <fabioborgesouzajunior@gmail.com>
2024-02-19 18:54:36 -0300

    :lipstick: style: Linter in Reference component

--- src/components/Reference/Reference.tsx
+++ src/components/Reference/Reference.tsx
@@ -1,7 +1,6 @@
 'use client'
 
 import Link from 'next/link'
-import { Button } from '../ui/button'
 
 type ReferenceProps = {
   data: string[]
@@ -9,12 +8,16 @@ type ReferenceProps = {
 
 export default function Reference(props: ReferenceProps) {
   return (
-    <div className='break-all container mt-8 flex flex-col bg-red-300 w-1/4'>
+    <div className="break-all container mt-8 flex flex-col bg-red-300 w-1/4">
       <h1>SINAPI</h1>
       {props.data.map((reference: string) => (
-          <Link key={reference} href={reference} className='break-all mt-2 h-auto'>
-            <p className='break-words'>{reference.slice(94)}</p>
-          </Link>
+        <Link
+          key={reference}
+          href={reference}
+          className="break-all mt-2 h-auto"
+        >
+          <p className="break-words">{reference.slice(94)}</p>
+        </Link>
       ))}
     </div>
   )
