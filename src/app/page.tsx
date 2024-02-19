1aa00cb7350e0c72a654181c8f477edeb7c8392f
Fabio <fabioborgesouzajunior@gmail.com>
2024-02-18 21:53:09 -0300

    :sparkles: feat: Imports data from sinapi.txt to app component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,14 @@
 import Reference from '@/components/Reference/Reference'
+import fs from 'fs'
 
+fs.readFile('src/data/sinapi.txt', 'utf-8', (error, data) => {
+  try {
+    const dataFile = data.split('\n')
+    console.log(dataFile)
+  } catch {
+    console.error(error)
+  }
+})
 export default function Home() {
   return <Reference />
 }
